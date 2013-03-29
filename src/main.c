6918850d16aebe85830f2c2006bbeb042f6682a1
Peter Shaw <unthoughted@googlemail.com>
2013-03-29 14:09:01 +0100

    rename the application nanme in menu

--- src/main.c
+++ src/main.c
@@ -44,7 +44,7 @@ int main(void) {
     ui_menu_init();
     ui_menu_add("Say Hello", fn_sayHello );
     ui_menu_add("Count ext ports", fn_countExtPorts );
-    ui_menu_add("Count fast ports", fn_countFastPorts );
+    ui_menu_add("Show fast ports", fn_showFastPorts );
     ui_menu_add("Show DAC values", fn_showDACValue );
     menuentry_t *optionsMenu = ui_menu_add("Options:\n(Submenu)", dummy );
     ui_menu_add_sub(optionsMenu, "Send data via\nUART", opt_uartonoff_init );
