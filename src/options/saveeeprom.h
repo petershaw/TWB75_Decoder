b528489f79b82e15c8dfa6da8003c8dd298fc0df
Peter Shaw <unthoughted@googlemail.com>
2013-04-09 22:27:11 +0200

    resolve conflicts
    clean up different versions

--- src/options/saveeeprom.h
+++ src/options/saveeeprom.h
@@ -9,10 +9,6 @@
 #ifndef AnalyserDude_saveeeprom_h
 #define AnalyserDude_saveeeprom_h
 
-<<<<<<< HEAD
-void *opt_save_preferences(void);
-=======
 void *opt_save_preferences(int init);
->>>>>>> add cron & preference saving
 
 #endif
